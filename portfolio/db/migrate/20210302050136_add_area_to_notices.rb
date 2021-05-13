f8fe7c15764909308dc3fbf5753aabfb464bfd1f
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-05-13 15:40:38 +0900

    3

--- portfolio/db/migrate/20210302050136_add_area_to_notices.rb
+++
@@ -1,5 +0,0 @@
-class AddAreaToNotices < ActiveRecord::Migration[6.1]
-  def change
-    add_column :notices, :area, :integer, default: 0
-  end
-end
--- portfolio/db/migrate/20210309141321_add_column_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :area, :integer, default: 0
-    add_column :users, :introduction, :text
-  end
-end
