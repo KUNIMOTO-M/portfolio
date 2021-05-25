0e6f2f6994225603a03d822476995400a3ecd6ce
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-05-26 01:52:14 +0900

    devide viewを追加

--- portfolio/app/models/user.rb
+++ portfolio/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+				 :confirmable, :omniauthable, omniauth_providers:[:twitter,:facebook]
 	mount_uploader :image, ImageUploader
 	has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
